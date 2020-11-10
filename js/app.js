c9c50d69e1e59c7d398a40e6262cb15168007e1c
James Gerstenberger <gerstej9@gmail.com>
2020-11-09 16:04:29 -0800

    Add contributors to README set up radio buttons on index to account for checks complete javascript functions except for showing results

--- js/app.js
+++ js/app.js
@@ -1,57 +1,119 @@
+//TODO
+//Need to figure out how to use radio buttons for adding votes
+// Need to render current vote tally 
+//Need to track how many times shown
+//Need to calculate percentage of times voted vs. shown
+//View Results button
+//25 round limit
+//
+
+var radioOne = document.getElementById("selection-one");
+var radioTwo = document.getElementById("selection-two");
+var radioThree = document.getElementById("selection-three");
+var indexOne;
+var indexTwo;
+var indexThree
+var formEl = document.getElementById('form');
+var rounds = 1;
 var allProducts = [];
 var imageOne = document.getElementById("image-one");
 var imageTwo = document.getElementById("image-two");
 var imageThree = document.getElementById("image-three");
 
-var Product = function(name){
-this.imgSource = `img/${name}.jpg`;
-this.title = this.alt = name;
+var Product = function(input){
+this.imgSource = `img/${input}`;
+this.title = this.alt = input.substring(0, input.length-4);
+this.timesShown = 0;
+this.timesVoted = 0;
+this.percentChosen = (this.timesVoted/this.timesShown)*100;
 
 allProducts.push(this);
 }
 
-new Product('bag');
-new Product('banana');
-new Product('bathroom');
-new Product('boots');
-new Product('breakfast');
-new Product('bubblegum');
-new Product('chair');
-new Product('cthulhu');
-new Product('dog-duck');
-new Product('dragon');
-new Product('pen');
-new Product('pet-sweep');
-new Product('scissors');
-new Product('shark');
-new Product('sweep');
-new Product('tauntaun');
-new Product('unicorn');
-new Product('water-can');
-new Product('wine-glass');
-//Need to add .gif file
-// new Product('bag');
+new Product('bag.jpg');
+new Product('banana.jpg');
+new Product('bathroom.jpg');
+new Product('boots.jpg');
+new Product('breakfast.jpg');
+new Product('bubblegum.jpg');
+new Product('chair.jpg');
+new Product('cthulhu.jpg');
+new Product('dog-duck.jpg');
+new Product('dragon.jpg');
+new Product('pen.jpg');
+new Product('pet-sweep.jpg');
+new Product('scissors.jpg');
+new Product('shark.jpg');
+new Product('tauntaun.jpg');
+new Product('unicorn.jpg');
+new Product('water-can.jpg');
+new Product('wine-glass.jpg');
+new Product('sweep.png');
+new Product('usb.gif');
+
+
+
+
+
 
 function randomGenerator(){
     return Math.floor(Math.random() * allProducts.length);
 }
 
 function render(){
-    var indexOne = randomGenerator();
-    var indexTwo = randomGenerator();
-    var indexThree = randomGenerator();
+    indexOne = randomGenerator();
+    indexTwo = randomGenerator();
+    indexThree = randomGenerator();
+    while(indexTwo === indexOne){
+        indexTwo = randomGenerator();
+    }
+    while(indexThree === indexOne || indexThree === indexTwo){
+        indexThree = randomGenerator();
+    }
 
     imageOne.src = allProducts[indexOne].imgSource;
     imageOne.alt = allProducts[indexOne].alt;
     imageOne.alt = allProducts[indexOne].title;
+    allProducts[indexOne].timesShown++;
 
     imageTwo.src = allProducts[indexTwo].imgSource;
     imageTwo.alt = allProducts[indexTwo].alt;
     imageTwo.alt = allProducts[indexTwo].title;
+    allProducts[indexTwo].timesShown++;
 
     imageThree.src = allProducts[indexThree].imgSource;
     imageThree.alt = allProducts[indexThree].alt;
     imageThree.alt = allProducts[indexThree].title;
+    allProducts[indexThree].timesShown++;
 }
-console.log(allProducts);
+
+
+function voteMaster(event){
+    event.preventDefault();
+    if (radioOne.checked == true){
+        allProducts[indexOne].timesVoted++;
+    }
+    if (radioTwo.checked == true){
+        allProducts[indexTwo].timesVoted++;
+    }
+    if (radioThree.checked == true){
+        allProducts[indexThree].timesVoted++;
+    }
+
+    render();
+    radioOne.checked = false;
+    radioTwo.checked = false;
+    radioThree.checked = false;
+}
+
+
+
 render();
+
+
+
+
+
+formEl.addEventListener('submit', voteMaster)
+
+
