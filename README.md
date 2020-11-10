0422d97cfb32e2eb032a2628998c11e4bcc5690b
James Gerstenberger <gerstej9@gmail.com>
2020-11-09 17:26:16 -0800

    finished javascript working on cSS

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Lab 11 for Code 201
 * https://loremipsum.io/generator/?n=2&t=p
 https://stackoverflow.com/questions/31489413/remove-last-3-characters-of-string-or-number-in-javascript/45165923
 W3Schools
+Ta's Cait and Bade
 
 ## Author
 James Gerstenberger
--- js/app.js
+++ js/app.js
@@ -13,22 +13,30 @@ var radioThree = document.getElementById("selection-three");
 var indexOne;
 var indexTwo;
 var indexThree
-var formEl = document.getElementById('form');
-var rounds = 1;
+var formEl = document.getElementById("form");
+var resultsEl = document.getElementById("results")
+var resultsForm = document.getElementById("results-form")
+var rounds = 0;
 var allProducts = [];
 var imageOne = document.getElementById("image-one");
 var imageTwo = document.getElementById("image-two");
 var imageThree = document.getElementById("image-three");
+
+
+
 
 var Product = function(input){
 this.imgSource = `img/${input}`;
 this.title = this.alt = input.substring(0, input.length-4);
 this.timesShown = 0;
 this.timesVoted = 0;
-this.percentChosen = (this.timesVoted/this.timesShown)*100;
+// this.percentChosen = parseInt(this.timesVoted/this.timesShown*100);
 
 allProducts.push(this);
 }
+
+
+
 
 new Product('bag.jpg');
 new Product('banana.jpg');
@@ -85,6 +93,7 @@ function render(){
     imageThree.alt = allProducts[indexThree].alt;
     imageThree.alt = allProducts[indexThree].title;
     allProducts[indexThree].timesShown++;
+    rounds++
 }
 
 
@@ -99,14 +108,35 @@ function voteMaster(event){
     if (radioThree.checked == true){
         allProducts[indexThree].timesVoted++;
     }
-
-    render();
-    radioOne.checked = false;
+    resultsEl.innerHTML = '';
+    if(rounds < 26){
+        render();
+    }else{
+        formEl.innerHTML = '';
+        createH2 = document.createElement('h2');
+        createH2.textContent('Thank you for your help! Click on the "generate results" button to see your contribution');
+        formEl.appendChild(createH2);
+    }
+    radioOne.checked = false; 
     radioTwo.checked = false;
     radioThree.checked = false;
 }
 
-
+function showResults(event){
+    event.preventDefault();
+    resultsEl.innerHTML = '';
+    var perecentChosen = 0
+    for(var i = 0; i < allProducts.length; i++){
+        if(allProducts[i].timesVoted === 0 && allProducts[i].timesShown === 0){
+            percentChosen = 0
+        }else {
+            percentChosen = allProducts[i].timesVoted/allProducts[i].timesShown*100
+        }
+        var createLi = document.createElement('li');
+        createLi.textContent = (`${allProducts[i].title}     ${allProducts[i].timesShown}     ${allProducts[i].timesVoted}     ${percentChosen}%`);
+        resultsEl.appendChild(createLi);
+    }
+}
 
 render();
 
@@ -114,6 +144,8 @@ render();
 
 
 
-formEl.addEventListener('submit', voteMaster)
+formEl.addEventListener('submit', voteMaster);
+
+resultsForm.addEventListener('submit',showResults);
 
 
